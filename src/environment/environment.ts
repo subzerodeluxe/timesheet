c654c2b35be536cd8f3540f5bc7d05c8e097b83f
Maarten <maarten.waanders@gmail.com>
2020-02-07 21:41:42 +0100

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    production: false,
-    firebase: {
-        apiKey: 'AIzaSyCIWqeaN5Twmgx4Jnaouey40fkrxZEU7QY',
-        authDomain: 'test-ce9c6.firebaseapp.com',
-        databaseURL: 'https://test-ce9c6.firebaseio.com',
-        projectId: 'test-ce9c6',
-        storageBucket: 'test-ce9c6.appspot.com',
-        messagingSenderId: '42673166780'
-    }
-  };
+    production: false
+};
